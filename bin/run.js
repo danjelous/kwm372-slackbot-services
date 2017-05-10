627ad4a785822ed6e3554e3b3bd09e36ac3237c9
Danjelous <daniel.brunner@live.at>
2017-05-10 15:46:42 +0200

    Add service registration

--- bin/run.js
+++ bin/run.js
@@ -3,10 +3,28 @@
 const http = require('http');
 const config = require('../config');
 const log = config.log();
+const request = require('superagent');
 const service = require('../server/service')(config);
 const server = http.createServer(service);
-server.listen(process.env.port || 3001);
+
+// Take a port chosen by node
+server.listen();
 
 server.on('listening', function(){
     log.info(`Time service is listening on ${server.address().port} in ${service.get('env')} mode.`);
+
+    // Quick n dirty
+    const mainUrl = 'http://127.0.0.1:3000';
+
+    const announce = () => {
+        request.put(`${mainUrl}/service/time/${server.address().port}`)
+        .end((err) => {
+            if(err) log.error(err);
+        })
+    }
+
+    announce();
+
+    // All 20s; timeout 30s
+    setInterval(announce, 20 * 1000);
 });
